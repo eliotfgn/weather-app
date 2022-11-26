d52279125f7de10fd3c8cf6c070eac10e7a6b569
Eliot Fagnon <efagnon2002@gmail.com>
2022-11-26 16:40:30 +0100

    position retrieved during loading

--- lib/pages/loading_screen.dart
+++ lib/pages/loading_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/pages/location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -10,11 +14,36 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+
+    getPosition();
+  }
+
+  void getPosition() async {
+    Position position;
+
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      Geolocator.getCurrentPosition().then((value) => Navigator.push(
+          context, MaterialPageRoute(builder: (context) => LocationScreen())));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: SpinKitFadingCube(
-        color: Colors.red,
-        size: 50,
+    return const SafeArea(
+      child: Center(
+        child: SpinKitRipple(
+          color: Colors.red,
+          size: 100,
+        ),
       ),
     );
   }
